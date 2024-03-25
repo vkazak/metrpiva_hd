0ec9b2ced9bb6252449aad8649c99066cec9e0fd
Viktor Kazak <vikt.kazak@gmail.com>
2024-03-25 21:09:53 +0300

    Fix tg logo path

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     return <div className='flex justify-center items-center gap-6 opacity-30 hover:opacity-80 transition-opacity h-20'>
         <p className='text-sm'>v {packageJson?.version}</p>
         <div className='flex items-center gap-1'>
-            <Image className='h-12' src='./tg-logo.svg' />
+            <Image className='h-12' src='/tg-logo.svg' />
             <p className='text-sm'>@Victor_k</p>
         </div>
     </div>
